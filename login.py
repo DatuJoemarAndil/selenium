3539a6aa1ffaf42ba69d7e29a88b50e281369453
AndilDatuJoemar <djandil15@gmail.com>
2025-03-20 12:20:05 +0800

    changed

--- login.py
+++ login.py
@@ -17,20 +17,20 @@ driver.maximize_window()
 wait = WebDriverWait(driver, 30)
 
 
-time.sleep(1)
+time.sleep(15)
 email_input = wait.until(EC.presence_of_element_located((By.ID, "email")))
 email_input.send_keys("09318125600")
 
 
-time.sleep(1)
+time.sleep(15)
 password_input = wait.until(EC.presence_of_element_located((By.ID, "pass")))
 password_input.send_keys( "marjoe")
 
 
-time.sleep(1)
+time.sleep(15)
 login_button = wait.until(EC.element_to_be_clickable((By.NAME, "login")))
 login_button.click()
 
 
-time.sleep(50)
+time.sleep(100)
 driver.quit()
